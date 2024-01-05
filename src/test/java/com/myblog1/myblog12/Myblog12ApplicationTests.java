d314a06b42849f916546a6cde574e2a22e79538e
charumathi <kcharumathi81@gmail.com>
2024-01-05 15:48:57 +0530

    127:Create post save feature

---
+++ src/test/java/com/myblog1/myblog12/Myblog12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog1.myblog12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
